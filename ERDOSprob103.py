90d8eb1c1412cfb31d27233c3ad98e5972dd7e3c
Gagan6164 <32809202+Gagan6164@users.noreply.github.com>
2019-10-02 20:08:45 +0530

    added new method

--- ERDOSprob103.py
+++ ERDOSprob103.py
@@ -15,3 +15,20 @@ for i in range(0,2**n,2):
         sum = sum + int(b)
     
 print(sum)
+
+#another method
+
+sum1 = 0
+for i in range(333333,777777):
+    s = str(i)+'3'
+    lst = list(s)
+    j = int(s)
+    n=0
+    for k in range(7):
+            if (lst[k]=='3' or lst[k]=='7'):
+                n = n+1
+            if (n ==7):
+                print(j)
+                if (j%7==0 and j%10==3):
+                    sum1 = sum1 + j
+print(sum1)
